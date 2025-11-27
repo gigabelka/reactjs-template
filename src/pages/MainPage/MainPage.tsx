87ec32a55e818942f0fe47380f300240ef917490
gigabelka <gigabelka@gmail.com>
2025-11-28 02:20:11 +0500

    Update routing to replace IndexPage with MainPage as the base component

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+import { Page } from '@/components/Page.tsx';
+
+export const MainPage: FC = () => {
+  return (
+    <Page back={false}>hello world
+    </Page>
+  );
+};
