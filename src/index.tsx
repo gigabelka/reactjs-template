2449631603f1c2b9e3e4d4aab0ae433db1e044e3
gigabelka <gigabelka@gmail.com>
2025-12-02 22:54:15 +0500

    Remove unused CSS import and clean up index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,9 @@ import '@telegram-apps/telegram-ui/dist/styles.css';
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import { retrieveLaunchParams } from '@tma.js/sdk-react';
-
 import { Root } from '@/components/Root.tsx';
 import { EnvUnsupported } from '@/components/EnvUnsupported.tsx';
 import { init } from '@/init.ts';
-
-import './index.css';
 
 // Mock the environment in case, we are outside Telegram.
 import './mockEnv.ts';
